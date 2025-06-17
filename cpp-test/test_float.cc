df078047a35da46af710574a6a048a74c7d8e22d
kaien <kaienkira@gmail.com>
2025-06-17 22:05:17 +0800

    work sync

--- cpp-test/test_float.cc
+++ cpp-test/test_float.cc
@@ -26,15 +26,31 @@ void printFloatInfo(const std::string &type_name)
     std::cout << std::format("has_signaling_NaN: {:s}",
         std::numeric_limits<T>::has_signaling_NaN)
         << std::endl;
-    std::cout << std::format("digits: {} digits10: {} max_digits10: {}",
-        std::numeric_limits<T>::digits,
-        std::numeric_limits<T>::digits10,
+    std::cout << std::format("digits: {}",
+        std::numeric_limits<T>::digits)
+        << std::endl;
+    std::cout << std::format("digits10: {}",
+        std::numeric_limits<T>::digits10)
+        << std::endl;
+    std::cout << std::format("max_digits10: {}",
         std::numeric_limits<T>::max_digits10)
+        << std::endl;
+    std::cout << std::format("min: {}",
+        std::numeric_limits<T>::min())
+        << std::endl;
+    std::cout << std::format("max: {}",
+        std::numeric_limits<T>::max())
+        << std::endl;
+    std::cout << std::format("lowest: {}",
+        std::numeric_limits<T>::lowest())
         << std::endl;
 }
 
 void printFloatBit()
 {
+    float min = std::numeric_limits<float>::min();
+    float lowest = std::numeric_limits<float>::lowest();
+    float max = std::numeric_limits<float>::max();
     float inf =
         std::numeric_limits<float>::infinity();
     float negative_inf = -inf;
@@ -50,51 +66,67 @@ void printFloatBit()
     float qnan1 = std::nanf("1");
     float qnan2 = std::nanf("2");
 
-    std::cout << std::format("{:10} {:032b}",
-        inf,
+    constexpr int col_1_width = 7;
+
+    std::cout << std::format("{:>{}} {:032b} {}",
+        "min", col_1_width,
+        *reinterpret_cast<uint32_t *>(&min),
+        min)
+        << std::endl;
+    std::cout << std::format("{:>{}} {:032b} {}",
+        "max", col_1_width,
+        *reinterpret_cast<uint32_t *>(&max),
+        max)
+        << std::endl;
+    std::cout << std::format("{:>{}} {:032b} {}",
+        "lowest", col_1_width,
+        *reinterpret_cast<uint32_t *>(&lowest),
+        lowest)
+        << std::endl;
+    std::cout << std::format("{:{}} {:032b}",
+        inf, col_1_width,
         *reinterpret_cast<uint32_t *>(&inf))
         << std::endl;
-    std::cout << std::format("{:10} {:032b}",
-        negative_inf,
+    std::cout << std::format("{:{}} {:032b}",
+        negative_inf, col_1_width,
         *reinterpret_cast<uint32_t *>(&negative_inf))
         << std::endl;
-    std::cout << std::format("{:10} {:032b}",
-        zero,
+    std::cout << std::format("{:{}} {:032b}",
+        zero, col_1_width,
         *reinterpret_cast<uint32_t *>(&zero))
         << std::endl;
-    std::cout << std::format("{:10} {:032b}",
-        negative_zero,
+    std::cout << std::format("{:{}} {:032b}",
+        negative_zero, col_1_width,
         *reinterpret_cast<uint32_t *>(&negative_zero))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} nan_zero_divide_zero",
-        nan_zero_divide_zero,
+    std::cout << std::format("{:{}} {:032b} nan_zero_divide_zero",
+        nan_zero_divide_zero, col_1_width,
         *reinterpret_cast<uint32_t *>(&nan_zero_divide_zero))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} NAN",
-        nan_macro,
+    std::cout << std::format("{:{}} {:032b} NAN",
+        nan_macro, col_1_width,
         *reinterpret_cast<uint32_t *>(&nan_macro))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} qnan",
-        qnan,
+    std::cout << std::format("{:{}} {:032b} qnan",
+        qnan, col_1_width,
         *reinterpret_cast<uint32_t *>(&qnan))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} snan",
-        snan,
+    std::cout << std::format("{:{}} {:032b} snan",
+        snan, col_1_width,
         *reinterpret_cast<uint32_t *>(&snan))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} qnan0",
-        qnan0,
+    std::cout << std::format("{:{}} {:032b} qnan0",
+        qnan0, col_1_width,
         *reinterpret_cast<uint32_t *>(&qnan0))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} qnan1",
-        qnan1,
+    std::cout << std::format("{:{}} {:032b} qnan1",
+        qnan1, col_1_width,
         *reinterpret_cast<uint32_t *>(&qnan1))
         << std::endl;
-    std::cout << std::format("{:10} {:032b} qnan2",
-        qnan2,
+    std::cout << std::format("{:{}} {:032b} qnan2",
+        qnan2, col_1_width,
         *reinterpret_cast<uint32_t *>(&qnan2))
         << std::endl;
-
 }
 
 int main(void)
